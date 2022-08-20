d0864deb405df8a26a66ddbd564ce00bbf1616b9
Revan More <65714331+revanasidd@users.noreply.github.com>
2022-08-20 14:06:44 +0530

    Update settings.py

--- djangopost/settings.py
+++ djangopost/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import django_heroku
 
 from pathlib import Path
 
@@ -150,5 +149,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-django_heroku.settings(locals())
